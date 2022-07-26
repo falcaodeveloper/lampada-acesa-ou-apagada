17bcca84d0384267c0f541fe86fa3ff49677e288
Ulisses Falcão <ulisses.falcao@outlook.com>
2022-07-26 16:02:41 -0300

    commit

---
+++ meuScript.js
@@ -0,0 +1,7 @@
+function acenderLampada() {
+    document.getElementById("id-lampada").src = "imagens/lampada-acesa-270.png";
+}
+
+function apagarLampada() {
+    document.getElementById("id-lampada").src = "imagens/lampada-apagada-270.png";
+}
